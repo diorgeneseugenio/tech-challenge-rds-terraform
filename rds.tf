9acf303ae4ebc07690ccc54b716de83b368a3d1e
Diórgenes Eugênio da Silveira <diorgeneseugenio@gmail.com>
2023-10-30 03:28:49 -0300

    feat: adiciona RDS no plano

---
+++ rds.tf
@@ -0,0 +1,16 @@
+resource "aws_db_instance" "fiap-soat-tech-challenge-db" {
+  identifier             = "fiap-soat-tech-challenge-db"
+  engine                 = "mysql"
+  engine_version         = "5.7"
+  instance_class         = "db.t2.micro"
+  allocated_storage      = 10
+  db_name                = "fiap_soat_tech_challenge_db"
+  username               = "fiap_soat_tech_challenge"
+  password               = "fiapsoattechchallenge"
+  parameter_group_name   = "default.mysql5.7"
+  skip_final_snapshot    = true
+
+  tags = {
+    Name = "fiap-soat-tech-challenge-db"
+  }
+}
